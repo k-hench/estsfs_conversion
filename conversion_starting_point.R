06250fb6515e5c544dac121b6e60ca5307b40c23
k-hench <khench@posteo.net>
2022-09-14 18:27:59 +0200

    update

--- conversion_starting_point.R
+++ conversion_starting_point.R
@@ -3,7 +3,6 @@ library(rlang)
 data <- read_tsv("test_counts.tsv")
 
 digest_ac <- function(str){
-#  str <- enexpr(str)
   A = 0; C = 0; G = 0; T = 0
   eval(parse(text = str))
   glue::glue("{A},{C},{G},{T}")
